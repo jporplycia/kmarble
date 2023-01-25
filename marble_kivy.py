c03a97e044c4c112e48d76279bd51bb3c47bd0a1
Jaroslav Porplycia <jaroslav@porplycia.com>
2023-01-25 09:15:49 +0100

    lazení vzhledu formuláře, úpravy písma a rozložení obrazovky Nastavení

--- marble_kivy.py
+++ marble_kivy.py
@@ -1,25 +1,28 @@
 # File name: marble_kivy.app
 #:kivy 2.1.0
 from kivy.app import App
+from kivymd.app import MDApp
 from kivy.uix.pagelayout import PageLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+from kivy.core.text import LabelBase
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 
-class ObrazovkaHry(PageLayout):
-    def switch_to(self, page):
-        self.page = page
+class ObrazovkaHry(Screen):
+    pass
 
+class ObrazovkaNastaveni(Screen):
+    pass
+        
 class Pozadi(BoxLayout):
     pass
 
-class MojeTlacitko(Button):
-    pass
-
-class MarbleApp(App):
+class MarbleApp(MDApp):
     def build(self):
-        return ObrazovkaHry()
-    
-
+        sm = ScreenManager(transition=FadeTransition())
+        sm.add_widget(ObrazovkaHry(name='obrazovka_hry'))
+        sm.add_widget(ObrazovkaNastaveni(name='obrazovka_nastaveni'))
+        return sm
 
 if __name__ == '__main__':
+    LabelBase.register(name='Roboto', fn_regular='Roboto-Medium.ttf')
     MarbleApp().run()
